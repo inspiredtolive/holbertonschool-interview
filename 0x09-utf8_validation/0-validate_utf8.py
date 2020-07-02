907bd594e21c994b5ae6dcbfbd25c60a499ea871
Marco Chan <inspiredtolive0@gmail.com>
2020-07-02 00:43:55 +0000

    Simplifies solution and documents module

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
+"""Provides a function to validate UTF-8."""
 
 
 def validUTF8(data):
-    """Checks if the data is valid UTF-8."""
-    num_bytes = 0
-    for n in data:
-        mask = 1 << 7
-        if not num_bytes:
-            while mask & n:
-                num_bytes += 1
-                mask >>= 1
-            if not num_bytes:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
-                return False
-        else:
-            if n >> 6 != 0b10:
-                return False
-        num_bytes -= 1
-    return num_bytes == 0
+    """Determines if the data is valid UTF-8."""
+    try:
+        bytes(x & 0xff for x in data).decode()
+        return True
+    except UnicodeDecodeError:
+        return False
